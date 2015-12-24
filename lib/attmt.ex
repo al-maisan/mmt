f2235b3f70ea1cb6e2cb9efeb5919eeb838e87ad
Muharem Hrnjadovic <mh@cloud1.it>
2015-12-24 08:40:38 +0100

    improved license

--- lib/attmt.ex
+++ lib/attmt.ex
@@ -1,6 +1,26 @@
 defmodule Attmt do
   @moduledoc """
   Functionality revolving around attachments
+
+  mmt is a simple utility that allows the automated sending of emails
+  using a configuration file and a template for the email body. It was
+  written in Elixir and used mainly on linux systems like Arch Linux
+  and Ubuntu.
+  Copyright (C) 2015-2016  Muharem Hrnjadovic <muharem@linux.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
   """
 
   @doc """
